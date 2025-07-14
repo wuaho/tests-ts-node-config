535add355cdba1379c0bb61e9140638f3ecbdddb
wuaho <juanjoserequenalama@gmail.com>
2025-07-14 21:45:01 +0200

    Add config files

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import jestPlugin from 'eslint-plugin-jest';
+
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+  plugins: { jest: jestPlugin },
+  rules: {
+    ...jestPlugin.configs.recommended.rules,
+  },
+});
